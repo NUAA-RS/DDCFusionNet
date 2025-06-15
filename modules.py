17be6405b7607026dbbcec94d361361497a7c15a
NUAA_RS <103399914+NUAA-RS@users.noreply.github.com>
2025-06-15 17:17:26 +0800

    Update modules.py

--- modules.py
+++ modules.py
@@ -1,3 +1,3 @@
-# This is the official implementation code for the paper "DDCFusionNet: A Dual-Domain Parallel Feature Fusion Network for Infrared and Visible Image Fusion".
+# This is the official implementation code for the paper "![](./topic.png)".
 
 # The complete code will be organized and uploaded after the article has been accepted. 
